4cc7f82e997591c938278251f6ffeecc7eee9a6d
Israel Brown <darrylbrown15@gmail.com>
2019-11-25 22:20:27 +0000

    created two functions

---
+++ index.js
@@ -0,0 +1,17 @@
+//let create some variales to use in this Code
+var boy = false;
+var girl = false;
+var girlAge = 20;
+var boyAge = 30;
+
+var ages = [22, 32];
+function onStart() {
+  console.log("DataBase is running!!");
+}
+onStart();
+
+function onCreate(getAge) {
+  return this.getAge = ages;
+}
+ages.push(40);
+console.log(ages);
