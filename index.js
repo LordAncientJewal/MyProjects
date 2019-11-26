0723f8b9bb00d46b0b9018c38ffb462dff1bc19e
Israel Brown <darrylbrown15@gmail.com>
2019-11-26 21:56:42 +0000

    inserted a car engine..

--- index.js
+++ index.js
@@ -15,3 +15,27 @@ function onCreate(getAge) {
 }
 ages.push(40);
 console.log(ages);
+//creating a if statement to return true or false;
+//Create a Car Engine using if statements
+var engineStart = true;
+var engineOff = true;
+var engineSpeed = 45;
+
+function drive(paddel) {
+  this.paddel = engineStart;
+  if(paddel === true) {
+    return true;
+  }
+  return false;
+}
+engineStart = drive();
+
+//Turning the engine off.!!
+function driveStop(brake) {
+this.brake = engineOff;
+if(engineSpeed <= 3.5) {
+  return true + " Your engine is off!";
+}
+  return false + " Your engine is not off!";
+}
+engineSpeed = driveStop();
